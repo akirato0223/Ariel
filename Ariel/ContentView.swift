ed3bb1f0cc19bba5d61208493c8ba1fe3e4fd5fd
akirato0223 <58621138+akirato0223@users.noreply.github.com>
2020-01-16 20:53:53 +0900

    Initial Commit

---
+++ Ariel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ariel
+//
+//  Created by あきら on 1/16/20.
+//  Copyright © 2020 Akira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
